de77fb01963f9f174b1decaa74fae6241f69932f
shobana <shobana.sekar@vanavil-systems.com>
2019-07-25 15:02:22 +0530

    Constants change

--- userRoutes/Constant.js
+++ userRoutes/Constant.js
@@ -2,7 +2,8 @@
 module.exports = {
 
     test: 'Dummy message',
-    test1: 'Dummy message'
+    test1: 'Dummy message',
+    test2: 'Dummy Message'
 }
 // const test="Dummy message";
 // module.exports= test;
