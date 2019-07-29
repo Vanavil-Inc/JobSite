b27cc21932579e71a22b1b8ba6b0a6cc19c24fde
Shashikuamr <shashikumar.niranjan@vanavil-systems.com>
2019-07-29 18:13:37 +0530

    Enabling CROS

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 const config = require('./database');
 const routes = require('./routes');
+const cors = require('cors')
 
 const mongoose = require('mongoose');
 var bodyParser = require('body-parser');
@@ -9,13 +10,6 @@ var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-//   });
-
-// app.use(express.static(__dirname + '/'));
     
 //connection to database
 mongoose.connect(config.db, { useNewUrlParser: true });
@@ -28,10 +22,11 @@ mongoose.connection.on('error', (err) => {
 console.log('Database error:' + err);
 });
 
+//Enabling cors
+app.use(cors());
+
 // Connect all our routes to our application
 app.use('/', routes);
-
-  
 
 // Turn on that server!
 app.listen(8081, () => {
