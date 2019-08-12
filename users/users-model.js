53043b87f2276f6ed0204d10aaa9a1ef217959b2
Shashikuamr <shashikumar.niranjan@vanavil-systems.com>
2019-08-12 15:24:19 +0530

    table name

--- users/users-model.js
+++ users/users-model.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
 
-const JobSeeker = mongoose.model('JobSeeker', require('./users-schema'));
+const users = mongoose.model('users', require('./users-schema'));
 
-module.exports = JobSeeker;
+module.exports = users;
