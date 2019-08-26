73e8207b7f88c5c57939d48647397f4bb7ad4d96
shobana <shobana.sekar@vanavil-systems.com>
2019-08-26 15:25:50 +0530

    JobPost changes

--- recentjobs/recentjobs-routes.js
+++ recentjobs/recentjobs-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-const recentjobs = require('./recentjobs-model'); 
+const recentjobs = require('./recentjobs-model');
 
 router.route('/jobpost').post((req, res) => {
     console.log(req.body);
@@ -11,106 +11,127 @@ router.route('/jobpost').post((req, res) => {
     const Experience = req.body.Experience;
     const JobDetails = req.body.JobDetails;
 
-
-    var jobsObj = {        
-        JobNo : JobNo,        
-        JobTitle : JobTitle,        
-        Qualification : Qualification,
-        PrimarySkills : PrimarySkills,
-        Experience : Experience,  
-        JobDetails : JobDetails   
-    }
-
-    recentjobs.create(jobsObj, (err, jobs) => {
+    recentjobs.find({
+        JobNo: JobNo
+    }, function (err, response) {
         if (err) {
+            console.log(err)
             res.json({
                 success: false,
-                message: 'Not Submitted',
+                message: 'Something went wrong',
                 error: err
+            })
+        }
+        if (response.length > 0) {
+            res.json({
+                success: false,
+                message: 'Job No already Exists',
+                error: err
+            })
+        } else {
+            var jobsObj = {
+                JobNo: JobNo,
+                JobTitle: JobTitle,
+                Qualification: Qualification,
+                PrimarySkills: PrimarySkills,
+                Experience: Experience,
+                JobDetails: JobDetails
+            }
+
+            recentjobs.create(jobsObj, (err, jobs) => {
+                if (err) {
+                    res.json({
+                        success: false,
+                        message: 'Not Submitted',
+                        error: err
+                    });
+                }
+
+                res.json({
+                    success: true,
+                    message: "Job Posted Successfully",
+                    result: jobs
+                });
+
             });
         }
-
-        res.json({
-            success: true,
-            message: "Job Posted Successfully",
-            result: jobs
-        });
-
-    });
+    })
 });
 
 router.route('/getallrecentjobs').get((req, res) => {
-    recentjobs.find({}, function(err, response){
+    recentjobs.find({}, function (err, response) {
         if (err) {
             res.json({
                 success: false,
                 message: 'Jobs not found',
                 error: err
             });
-        } 
-            if(response.length > 0){
-                res.json({
-                    success: true,
-                    message: 'Recent Jobs Found',
-                    result: response
-                });
-                }else {
-                res.json({
-                    success: false,
-                    message: "No Recent Jobs",
-                    error: err
-                });
-             }
+        }
+        if (response.length > 0) {
+            res.json({
+                success: true,
+                message: 'Recent Jobs Found',
+                result: response
+            });
+        } else {
+            res.json({
+                success: false,
+                message: "No Recent Jobs",
+                error: err
+            });
+        }
     })
 });
 
 router.route('/deletejob').post((req, res) => {
     const JobNo = req.body.JobNo;
 
-        // console.log("userid"+UserId);
-        console.log(req.body);
-        recentjobs.findOneAndDelete({
-            JobNo : JobNo},function(err, response){
-                if (err) {
-                    res.json({
-                        success: false,
-                        message: 'Job not deleted',
-                        error: err
-                    });
-                }
-                if(response != null){
-                    res.json({
-                        success: true,
-                        message: "Deleted successfully",
-                        result: response
-                    }); 
-                } else {
-                    console.log("Job Not Found");
-                    res.json({
-                        success: false,
-                        message: 'Job Not Found',
-                        error: err
-                    });
-                }
-            })
+    // console.log("userid"+UserId);
+    console.log(req.body);
+    recentjobs.findOneAndDelete({
+        JobNo: JobNo
+    }, function (err, response) {
+        if (err) {
+            res.json({
+                success: false,
+                message: 'Job not deleted',
+                error: err
+            });
+        }
+        if (response != null) {
+            res.json({
+                success: true,
+                message: "Deleted successfully",
+                result: response
+            });
+        } else {
+            console.log("Job Not Found");
+            res.json({
+                success: false,
+                message: 'Job Not Found',
+                error: err
+            });
+        }
+    })
 });
 router.route('/updatejob').put((req, res) => {
     const JobNo = req.body.JobNo;
     recentjobs.findOneAndUpdate({
-        JobNo : JobNo},req.body,function(err, response){
+        JobNo: JobNo
+    }, req.body, function (err, response) {
         if (err) {
             res.json({
                 success: false,
                 message: 'Job not updated',
                 error: err
             });
-        } 
-        if(response != null){
+        }
+        if (response != null) {
             res.json({
                 success: true,
                 message: "Job updated successfully",
                 result: response
-            }); 
+            });
         } else {
             console.log("Job Not Found");
             // console.log(err);
@@ -119,7 +140,7 @@ router.route('/updatejob').put((req, res) => {
                 message: "Job not updated",
                 error: err
             });
-        }   
+        }
     })
 });
 
